5faab00edd1ec9f52eb4c79a9081c78bed520e16
Marco Castope <marcocastope7@gmail.com>
2018-11-18 15:36:54 -0500

    Se agrega redux y las rutas de la aplicación

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
-import Prueba from './components/Prueba';
-import Prueba2 from './components/Prueba2';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Layout from './components/Layout/Layout';
+import Movies from './containers/Movies/Movies';
+import CreateMovie from './containers/CreateMovie/CreateMovie';
+import EditMovie from './containers/EditMovie/EditMovie';
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Holassa {this.props.name}</h1>
-				<Prueba />
-				<Prueba2 />
-			</div>
+			<Provider store={store} >
+				<Router>
+					<div className="App" >
+						<Layout>
+							<Route exact path="/" component={Movies} />
+							<Route exact path="/create-movie" component={CreateMovie} />
+							<Route exact path="/edit-movie/:id" component={EditMovie} />
+						</Layout>
+					</div>
+				</Router>
+			</Provider>
 		)
 	}
 }

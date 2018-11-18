20dd56e88675176dadb9addd839869ec31a6dbbd
Marco Castope <marcocastope7@gmail.com>
2018-11-18 15:35:22 -0500

    Se agrega el archivo principal de react

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
 
-render(<App name="Marco" />, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
